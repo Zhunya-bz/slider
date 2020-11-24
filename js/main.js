672e788d2186d0f7b9c3ec48d2030dd39eb80a3b
Zhunya-bz <fluffylion2013@gmail.com>
2020-11-24 15:59:24 +0700

    create: only slider with keyboard

---
+++ js/main.js
@@ -0,0 +1,31 @@
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  //direction: 'vertical',
+  loop: true,
+
+  // If we need pagination это точки внизу
+ /* pagination: {
+    el: '.swiper-pagination',
+  },*/
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button_next',
+    prevEl: '.slider-button_prev',
+  },
+
+  // And if we need scrollbar
+  /*scrollbar: {
+    el: '.swiper-scrollbar',
+  },*/
+  speed: 600,
+  effect: 'coverflow',
+  grabCursor: true,
+  touchAngle: 45,
+  watchSlidesProgress: true,
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
